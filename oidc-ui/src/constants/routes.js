980725513102c3d84fdb619c1cd04056eb62b438
dhanendras <dhanendra.chaudhary@gmail.com>
2024-05-30 16:55:03 +0530

    as

--- oidc-ui/src/constants/routes.js
+++ oidc-ui/src/constants/routes.js
@@ -1,32 +1,32 @@
 // Authorization endpoint
-export const SEND_OTP = "/authorization/send-otp";
-export const AUTHENTICATE = "/authorization/v2/authenticate";
-export const AUTHENTICATE_V3 = "/authorization/v3/authenticate";
-export const OAUTH_DETAIL = "/authorization/v2/oauth-details";
-export const AUTHCODE = "/authorization/auth-code";
-export const CSRF = "/csrf/token";
+export const SEND_OTP = "/ui/authorization/send-otp";
+export const AUTHENTICATE = "/ui/authorization/v2/authenticate";
+export const AUTHENTICATE_V3 = "/ui/authorization/v3/authenticate";
+export const OAUTH_DETAIL = "/ui/authorization/v2/oauth-details";
+export const AUTHCODE = "/ui/authorization/auth-code";
+export const CSRF = "/ui/csrf/token";
 
 // Config endpoint
-export const CONFIG = "/theme/config.json";
+export const CONFIG = "/ui/theme/config.json";
 
 // Language endpoint
-export const DEFAULT_CONFIG = "/locales/default.json";
-export const ENG_CONFIG = "/locales/en.json";
+export const DEFAULT_CONFIG = "/ui/locales/default.json";
+export const ENG_CONFIG = "/ui/locales/en.json";
 
 // Link authorization endpoint
-export const LINK_CODE_GENERATE = "/linked-authorization/link-code";
-export const LINK_STATUS = "/linked-authorization/link-status";
-export const LINK_AUTHORIZATION_CODE = "/linked-authorization/link-auth-code";
+export const LINK_CODE_GENERATE = "/ui/linked-authorization/link-code";
+export const LINK_STATUS = "/ui/linked-authorization/link-status";
+export const LINK_AUTHORIZATION_CODE = "/ui/linked-authorization/link-auth-code";
 
 // SBI endpoint
-export const DEVICE = "/device";
-export const INFO = "/info";
-export const CAPTURE = "/capture";
+export const DEVICE = "/ui/device";
+export const INFO = "/ui/info";
+export const CAPTURE = "/ui/capture";
 
 // Route endpoint
-export const SOMETHING_WENT_WRONG = "/something-went-wrong";
-export const PAGE_NOT_FOUND = "/page-not-found"
-export const LOGIN = "/login";
-export const AUTHORIZE = "/authorize";
-export const CONSENT = "/consent";
-export const ESIGNET_DETAIL = "/";
+export const SOMETHING_WENT_WRONG = "/ui/something-went-wrong";
+export const PAGE_NOT_FOUND = "/ui/page-not-found"
+export const LOGIN = "/ui/login";
+export const AUTHORIZE = "/ui/authorize";
+export const CONSENT = "/ui/consent";
+export const ESIGNET_DETAIL = "/ui/";
